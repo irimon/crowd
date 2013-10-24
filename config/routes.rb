24c08a5d74697d66092c721b19196ce2f6c4fcf5
Ilan Rimon <ilanrimon@yahoo.com>
2013-10-23 20:55:08 -0400

    removed company paths

--- config/routes.rb
+++ config/routes.rb
@@ -22,7 +22,7 @@ CrowdMoc::Application.routes.draw do
       # :defaults => { :format => 'atom' }
   resources :relationships, only: [:create]
   resources :users
-  resources :companies
+  # resources :companies
   
   resources :news_items
 	match '/feed' => 'news_items#feed',
