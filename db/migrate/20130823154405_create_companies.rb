79db2613e61cfde97fb549fc58472c140d70f76d
Ilan Rimon <ilanrimon@yahoo.com>
2013-10-26 18:50:21 -0400

    changed description to text

--- db/migrate/20130823154405_create_companies.rb
+++ db/migrate/20130823154405_create_companies.rb
@@ -2,7 +2,7 @@ class CreateCompanies < ActiveRecord::Migration
   def change
     create_table :companies do |t|
       t.string :company_name
-      t.string :description
+      t.text :description
       t.string :website
 
       t.timestamps
