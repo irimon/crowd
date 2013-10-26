3e4f88be688845c7a228405f24897fae4ea8b27e
Ilan Rimon <ilanrimon@yahoo.com>
2013-10-26 17:50:26 -0400

    heroku db change

--- db/migrate/20130726192405_create_projects.rb
+++ db/migrate/20130726192405_create_projects.rb
@@ -2,7 +2,7 @@ class CreateProjects < ActiveRecord::Migration
   def change
     create_table :projects do |t|
       t.string :name
-      t.string :amount
+      t.integer :amount
 
       t.timestamps
     end 
--- db/migrate/20131010160208_change_amount_format_in_projects.rb
+++ db/migrate/20131010160208_change_amount_format_in_projects.rb
@@ -1,9 +1,9 @@
 class ChangeAmountFormatInProjects < ActiveRecord::Migration
   def self.up
-   change_column :projects, :amount, 'integer USING CAST(amount AS integer)'
+   #change_column :projects, :amount, 'integer USING CAST(amount AS integer)'
   end
 
   def self.down
-    change_column :projects, :amount, :string
+   # change_column :projects, :amount, :string
   end
 end
